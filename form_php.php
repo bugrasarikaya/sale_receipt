f36c683d0f78c7d8d86c3e59692289ce3cacbc9f
Buğra Sarıkaya <bugra_sarikaya@windowslive.com>
2021-08-04 21:56:40 +0000

    Files were updated but there are some errors.

--- form_php.php
+++ form_php.php
@@ -1,3 +1,25 @@
 <?php
-echo utf8_decode("������aa");
+if(empty($_POST)){
+    header("Location: form.html");
+    exit;
+}
+try{
+    $dsn="mysql:host=localhost;dbname=users;charset=utf8";
+    $conn=new PDO($dsn, "root","");
+}catch(PDOException $e){
+    print $e->getMessage();
+}
+$name=$_POST["name"];
+$surname=$_POST["surname"];
+$tel=$_POST["tel"];
+$email=$_POST["email"];
+$address=$_POST["address"];
+try{
+    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $sql="INSERT INTO users (name,surname,tel,email,address) VALUES ('$name','$surname','$tel','$email','$address');";
+    $conn->exec($sql);
+    header("Location: form.html");
+}catch(PDOException $e){
+    echo "<br>",$e->getMessage();
+}
 ?>
